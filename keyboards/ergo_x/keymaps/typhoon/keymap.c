530bf4a4ae524c73934a35c3ef8d15434a4c65fe
Chloe Schaff <47031895+nonbinarysolo@users.noreply.github.com>
2023-05-06 16:15:51 +0000

    Change L,R arrow keys back to normal

--- keyboards/ergo_x/keymaps/typhoon/keymap.c
+++ keyboards/ergo_x/keymaps/typhoon/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                                                      KC_ESC,         KC_F5,          KC_F4,          KC_F3,          KC_F2,          KC_F1,          LGUI(KC_L),
     KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                                                      KC_TRNS,        KC_F10,         KC_F9,          KC_F8,          KC_F7,          KC_F6,          KC_MAIL,
     KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                                                                                  KC_MUTE,        KC_UP,          SSHOT,          GCAP,           LCTL(KC_G),     KC_CALC,
-    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                                                      TO(1),          TD(RDESK),      KC_DOWN,        TD(LDESK),      GREC,           QK_BOOT,        KC_LCTL,
+    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                                                      TO(1),          KC_RIGHT,       KC_DOWN,         KC_LEFT,      GREC,           QK_BOOT,        KC_LCTL,
     KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,                                                                                              KC_F15,         KC_F14,         KC_F13,         KC_F12,         KC_F11,
                                                                                             KC_NO,      KC_NO,          KC_MNXT,        KC_MPRV,
                                                                                                         KC_NO,          KC_MPLY,
